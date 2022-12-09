b67a3a91e53233450cf8a4f1f0655ed53bfc27fd
Jadyn-Jenkins <Noblejadynjenkins@gmail.com>
2022-12-09 16:43:42 -0500

    Finalized creating table in js. organized populate.js and testtable.js. Updated link to js in todo.html

--- scripts/testtable.js
+++ scripts/testtable.js
@@ -1,56 +1,73 @@
-let table = document.getElementById('userTable');
-window.addEventListener("load", fetchDataMakeTable)
+const viewAllBtn = document.querySelector("#viewAllBtn");
+const searchBtn = document.querySelector('#searchBtn');
+const userIdDropDown = document.querySelector('#todoDropdown');
+const output = document.querySelector("#output");
 
-function fetchDataMakeTable() {
-    fetch('http://localhost:8083/api/todos')
-.then(console.log('to do list'))
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    for (let i = 0; i < data.length; i++) {
-        let row = table.insertRow(-1);
-        let userIdCol = row.insertCell(0);
-        console.log(data[i].userid);
-         userIdCol.innerHTML = data[i].userid;
+let allTodosEndPont = "http://localhost:8083/api/todos";
+let byUserEndpoint = "http://localhost:8083/api/todos/byuser/";
 
-         let desCol = row.insertCell(1);
-        console.log(data[i].description);
-        desCol.innerHTML = data[i].description;
+viewAllBtn.onclick = () => fetchData(allTodosEndPont);
+searchBtn.onclick = () => {
+    const id = userIdDropDown.value
+    if(id == "default") console.error("Choose an ID from the drop box");
+    else fetchData(`${byUserEndpoint}${id}`)
+}
+    //: fetchData(`${byUserEndpoint}${"user.id"}`);
 
-        let deadLineCol = row.insertCell(2);
-        console.log(data[i].deadline);
-        deadLineCol.innerHTML = data[i].deadline;
+function fetchData(endpoint) {
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      createTable(data);
+    });
+}
 
-        let categoryCol = row.insertCell(3);
-        console.log(data[i].category);
-        categoryCol.innerHTML = data[i].category;
+function createTable(data) {
+  let tableHeaders = [
+    "User ID",
+    "Description",
+    "DeadLine",
+    "Category",
+    "Priority",
+    "Completed",
+  ];
 
-        let priorityCol = row.insertCell(4);
-        console.log(data[i].priority);
-        priorityCol.innerHTML = data[i].priority;
+  let table = document.createElement("table");
+  let thead = document.createElement("thead");
+  table.appendChild(thead);
+  let tr = document.createElement("tr");
+  thead.appendChild(tr);
+//clear table
+output.innerHTML = "";
+  //Pop table header
+  for (let i = 0; i < tableHeaders.length; i++) {
+    let th = document.createElement("th");
+    th.textContent = tableHeaders[i];
+    tr.appendChild(th);
+  }
 
-        let completedCol = row.insertCell(5);
-        console.log(data[i].completed);
-        completedCol.innerHTML = data[i].completed;
-
-
-
-        /* <table id="userTable">
-    <thead>
-        <tr>
-            <th>User ID</th>
-            <th>Description</th>
-            <th>DeadLine</th>
-            <th>Category</th>
-            <th>Priority</th>
-            <th>Completed</th>
-        </tr>
-    </thead>
-    <tbody>
-      
-    </tbody> */
+  //popTableData
+  for (let i = 0; i < data.length; i++) {
+    let row = table.insertRow(-1);
     
-        
-    }
-})
+    let userIdCol = row.insertCell(0);
+    userIdCol.innerHTML = data[i].userid;
+
+    let desCol = row.insertCell(1);
+    desCol.innerHTML = data[i].description;
+
+    let deadLineCol = row.insertCell(2);
+    deadLineCol.innerHTML = data[i].deadline;
+
+    let categoryCol = row.insertCell(3);
+    categoryCol.innerHTML = data[i].category;
+
+    let priorityCol = row.insertCell(4);
+    priorityCol.innerHTML = data[i].priority;
+
+    let completedCol = row.insertCell(5);
+    completedCol.innerHTML = data[i].completed;
+  }
+  output.appendChild(table);
 }
